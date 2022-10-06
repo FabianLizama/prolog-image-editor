545b430db54889b21baa59d64883fedb0404db24
Fabián Lizama <fabian.lizama@usach.cl>
2022-10-06 00:32:41 -0300

    Se implementa TDA pixbit-d

---
+++ pixbit-d.pl
@@ -0,0 +1,45 @@
+%Dominios:
+% X: int >=0
+% Y: int >=0
+% D: int >=0
+% Bit: (0;1)
+% P: pixbit-d
+
+%Predicados
+%pixbit-d(X, Y, Bit, D, P).
+%getX(P, X).
+%getY(P, Y).
+%getBit(P, Bit).
+%getD(P, D).
+
+
+%Metas
+
+%Clausulas
+
+%Constructor
+pixbit-d(X, Y, Bit, D, [X, Y, Bit, D]):-
+	integer(X),
+	integer(Y),
+	integer(D),
+	D >= 0,
+	X >= 0,
+	Y >= 0,
+	(Bit == 0;Bit == 1).
+
+%Pertenencia
+ispixbit-d([X, Y, Bit, D|_]):-
+	pixbit-d(X, Y, Bit, D, [X, Y, Bit, D]).
+
+%Selectores
+getX([X|_], X).
+
+getY([_, Y|_], Y).
+
+getBit([_, _, Bit|_], Bit).
+
+getD([_, _, _, D|_], D).
+
+%Modificadores
+
+%Otros
